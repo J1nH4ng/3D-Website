6153c0eef0258b870747c1ac01529939ef5b02e5
J1nH4ng <j1nh4ng@icloud.com>
2025-02-13 22:13:31 +0800

    :fire: fix(target): 删除错误上传的调试文件

--- backend/target/debug/deps/backend-d87469aab52f725e.d
+++
@@ -1,5 +0,0 @@
-/data/codes/works/3D-Website/backend/target/debug/deps/libbackend-d87469aab52f725e.rmeta: src/main.rs
-
-/data/codes/works/3D-Website/backend/target/debug/deps/backend-d87469aab52f725e.d: src/main.rs
-
-src/main.rs:
